feec4003e85d9f867856f8f76fba35d70615be2a
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-10-20 21:32:17 +0800

    remove os

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
 # Simple web application using Flask framework
-#   - Use of Env Variables to configure
 from flask import Flask
-import os # (1) library for operating system calls
-
-PORT = os.getenv('PORT', 8080) # (2) get env
 
 app = Flask(__name__)
 
@@ -12,4 +8,4 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=PORT) # (3) pass port as an arg
+    app.run(host="0.0.0.0", port=8080)
